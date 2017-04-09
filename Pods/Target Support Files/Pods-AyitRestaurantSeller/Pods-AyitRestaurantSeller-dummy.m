753c3df3b55f78bf7ce5086c972f03f6c649fe87
Mark <mamian521@gmail.com>
2017-04-09 22:55:42 +0800

    removed pods folder

--- Pods/Target Support Files/Pods-AyitRestaurantSeller/Pods-AyitRestaurantSeller-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AyitRestaurantSeller : NSObject
-@end
-@implementation PodsDummy_Pods_AyitRestaurantSeller
-@end
--- Pods/Target Support Files/Pods-AyitRestaurantSellerTests/Pods-AyitRestaurantSellerTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AyitRestaurantSellerTests : NSObject
-@end
-@implementation PodsDummy_Pods_AyitRestaurantSellerTests
-@end

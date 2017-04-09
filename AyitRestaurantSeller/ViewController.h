161a205a044b11db6b051555d3aacf768a2ea0aa
Mark <mamian521@gmail.com>
2017-04-09 17:19:19 +0800

    Initial Commit

---
+++ AyitRestaurantSeller/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AyitRestaurantSeller
+//
+//  Created by MAMIAN on 2017/4/9.
+//  Copyright © 2017年 Gaofei Ma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
